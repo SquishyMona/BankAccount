40aed62a336e480489e9b58423d1c4c219044df1
Henry F Domst <>
2023-03-22 16:41:37 +0000

    bankAccount Constructor

---
+++ BankAccount.java
@@ -0,0 +1,14 @@
+//bankAccount constructor
+
+public class bankAccount {
+  int x;
+
+public Main(){
+  x = 
+}
+  public static void main(String[] args){
+    Scanner in = new bankAccount(1000)
+    System.out.println(bankAccount.x) //Prints bank account number
+      
+  }
+}
